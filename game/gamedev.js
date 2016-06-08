03bb28cda7ad15dac31a2e0b3bb58155fbf8c1e2
Superdog123 <210721@hkis.edu.hk>
2016-06-08 09:46:55 +0800

    blubber

---
+++ game/gamedev.js
@@ -0,0 +1,17 @@
+	$(document).ready(function(){
+
+$("#one").animate({right:'4100px'},6000);
+$("#two").animate({left:'4100px'},6000);
+$("#three").animate({right:'4100px'},6000);
+$("#four").animate({left:'4100px'},6000);
+$("#five").animate({right:'4100px'},6000);
+$("#six").animate({left:'4100px'},6000);
+$("#seven").animate({right:'4100px'},6000);
+$("#eight").animate({left:'4100px'},6000);
+$("#nein").animate({right:'4100px'},6000);
+$("#ten").animate({left:'4100px'},6000);
+
+});
+  $("#one").click(function(){
+    $("#one").hide();
+});
